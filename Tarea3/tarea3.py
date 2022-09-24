bd5c0c9f9bf08589a495d5334930f51d509a5964
PabloFdzC <pablof5181@gmail.com>
2022-09-24 16:04:06 -0600

    Tarea 3

--- Tarea3/tarea3.py
+++ Tarea3/tarea3.py
@@ -1,76 +1,179 @@
 import cv2
-import os
 import numpy as np
-import sys
 
+# Función para determinar si una función tiene inverso o no
+# si b * c - a * d es 0 significa que no tiene inverso, pero
+# si no es 0, significa que sí tiene.
 def mapeo_inverso_existe(a, b, c, d):
   result = b * c - a * d
   return result != 0
 
-# En este mapeo se usa el mapeo bilineal en su forma
-# lineal, o sea w = (az + b) / (cz + d) pasa a ser
-# w = a/c + (bc-ad)/(c(cz + d)) y eso se simplifica
-# a w = mu*z_2 +lambda
-# Entonces las variables que tiene son:
-# z_2 = 1 / z_1,
-# z_1 = alfa * z +beta, 
-# lambda = a/c,
-# mu = bc - ad,
-# alfa = c^2,
-# beta = c*d
-# Esto como se muestra en la página 36 del libro
-# Señales y Sistemas Fundamentos Matemáticos de
-# Pablo Alvarado Moya
-def mapeo_directo(imagen, a, b, c, d):
-    if not mapeo_inverso_existe(a,b,c,d):
-        return imagen
-    
-    (alto, ancho) = imagen.shape[:2]
+def calcula_tamanno(h, w, a, b, c ,d):
+  higherXPositive = 0
+  higherXNegative = 0
+  higherYPositive = 0
+  higherYNegative = 0
+  # En esta parte se calcula cuál es el tamaño
+  # de la imagen resultante, lo que se hace es 
+  # sumar el resultado más grande positivo y
+  # negativo para poder colocar en la imagen
+  # tanto los resultados positivos como negativos
+  # en un mismo plano
+  for x in range(h):
+    for y in range(w):
+      num = complex(x, y)
+      new =  (a * num + b) / (c * num + d)
+      if new.real >= 0:
+        if higherXPositive < round(new.real):
+          higherXPositive = round(new.real)
+      else:
+        if higherXNegative < abs(round(new.real)):
+          higherXNegative = abs(round(new.real))
+      if new.imag >= 0:
+        if higherYPositive < round(new.imag):
+          higherYPositive = round(new.imag)
+      else:
+        if higherYNegative < abs(round(new.imag)):
+          higherYNegative = abs(round(new.imag))
+  
+  return higherXPositive, higherXNegative, higherYPositive, higherYNegative
 
-    if c == 0:
-        lambda_ = complex(0)
-    else:
-        lambda_ = complex(a/c)
-    mu = complex(b*c-a*d)
-    cambioTamanno = np.linalg.norm([mu.real, mu.imag])
-    angulo = np.angle(mu)
-    nuevoTamanno = (round(ancho*cambioTamanno), round(alto*cambioTamanno))
-    res = cv2.resize(imagen,dsize=nuevoTamanno)
-    (alto, ancho) = res.shape[:2]
-    M = cv2.getRotationMatrix2D((lambda_.real, lambda_.imag), angulo, 1.0)
-    imagenNueva = cv2.warpAffine(src=res, M=M, dsize=(ancho, alto))
-    
-    return imagenNueva
 
-def mapeo_inverso(imagen, a, b, c, d):
-    if not mapeo_inverso_existe(a,b,c,d):
-        return imagen
-    
-    (alto, ancho) = imagen.shape[:2]
+# Función que representa al mapeo bilineal, 
+# genera el plano w con la imagen a través del plano z 
+# de a, b, c y d.
+def mapeo_bilineal(image, a, b, c, d):
+  # Primero se revisa si el mapeo inverso se puede hacer
+  if not mapeo_inverso_existe(a,b,c,d):
+    return image
 
-    alfa = complex(c)**2
-    if c == 0:
-        lambda_ = complex(0)
-    else:
-        lambda_ = complex(-d/c)
-    mu = complex(b*c-a*d)
-    cambioTamanno = np.linalg.norm([mu.real, mu.imag])
-    angulo = np.angle(mu)
-    nuevoTamanno = (round(ancho/cambioTamanno), round(alto/cambioTamanno))
-    res = cv2.resize(imagen,dsize=nuevoTamanno)
-    (alto, ancho) = res.shape[:2]
-    M = cv2.getRotationMatrix2D((lambda_.real, lambda_.imag), angulo, 1.0)
-    imagenNueva = cv2.warpAffine(src=res, M=M, dsize=(ancho, alto))
-    
-    return imagenNueva
+  (h, w) = image.shape[:2]
+  higherXPositive, higherXNegative, higherYPositive, higherYNegative = calcula_tamanno(h, w, a, b, c ,d)
+  wResult = np.zeros((higherXPositive + higherXNegative + 1, higherYPositive + higherYNegative + 1, 3), dtype = "uint8")
+  # Aquí se genera el plano w, utilizando la 
+  # fórmula del mapeo bilineal, lo que hace es 
+  # pintar de azul los puntos que corresponden 
+  # a un punto del plano z.
+  for x in range(h):
+    for y in range(w):
+      num = complex(x, y)
+      new =  (a * num + b) / (c * num + d)
+      newX = round(new.real)
+      newY = round(new.imag)
+      # Cuando el resultado es positivo se le
+      # suma el resultado del más grande negativo
+      # para que quede abajo de los negativos
+      if(newX >= 0):
+        newX = newX + higherXNegative
+      # Los negativos se convierten a la posición
+      # que les corresponde
+      else:
+        newX = higherXNegative - abs(newX)
+      if(newY >= 0):
+        newY = newY + higherYNegative
+      else:
+        newX = higherYNegative - abs(newY)
+      wResult[newX][newY] = image[x][y]
+  return wResult
 
-if len(sys.argv) < 5:
-    imagen = cv2.imread("Original.png")
-    imagen2 = mapeo_directo(imagen, 2.1+2.1j, 0, 0.003, 1+1j)
-    #imagen2 = mapeo(imagen, 4, 0, 0, 1)
-else:
-    imagen = cv2.imread(sys.argv[0])
-    imagen2 = mapeo_directo(imagen, complex(sys.argv[1]), complex(sys.argv[2]), complex(sys.argv[3]), complex(sys.argv[4]))
-    
+# Función que hace la transformación de la
+# imagen usando mapeo inverso.
+# Genera el plano w a través del plano z 
+# representado por una imagen y las constantes 
+# de a, b, c y d.
+def mapeo_inverso(image, a, b, c, d):
+  # Primero se revisa si el mapeo inverso se puede hacer
+  # debido a que se utiliza
+  if not mapeo_inverso_existe(a,b,c,d):
+    return image
 
+  (h, w) = image.shape[:2]
+  higherXPositive, higherXNegative, higherYPositive, higherYNegative = calcula_tamanno(h, w, a, b, c ,d)
+  h2 = higherXPositive + higherXNegative + 1
+  w2 = higherYPositive + higherYNegative + 1
+  wResult = np.zeros((h2, w2, 3), dtype = "uint8")
+  
+  # Mapeo de los puntos en el plano w
+  # tomando los resultados que se obtiene
+  # al sacar la inversa
+  for x in range(h2):
+    for y in range(w2):
+      num = complex(x, y)
+      inv =  (-d * num + b) / (c * num - a)
+      invX = round(inv.real)
+      invY = round(inv.imag)
+      if invX < h and invY < w and invX >= 0 and invY >= 0:
+        wResult[x][y] = image[invX][invY]
+
+  return wResult
+
+# Función para hacer la interpolación en
+# colindacia N, solo se hacen los calculos
+# si la N = 4 o N = 8 
+def interpolacion(image, N):
+  (h, w) = image.shape[:2]
+  wResult = np.zeros((h, w, 3), dtype = "uint8")
+  # Se crean las matrices para calcular el
+  # promedio solo con los valores deseados
+  if(N == 4):
+    vals = np.array([[0,1,0],[1,0,1],[0,1,0]]) / 4
+  elif(N == 8):
+    vals = np.array([[1,1,1],[1,0,1],[1,1,1]]) / 8
+  for x in range(h):
+    for y in range(w):
+      mean = 0
+      for xVals in range(vals.shape[0]):
+        for yVals in range(vals.shape[1]):
+          if vals[xVals][yVals] != 0:
+            imgX = x+xVals-1
+            imgY = y+yVals-1
+            # Se ajusta el X y Y de la imagen
+            # original para obtener los valores
+            # correctos, en este caso los bordes
+            # toman el valor que tenga el punto
+            # que se revisa actualmente
+            if imgX < 0 or imgX >= h-1:
+              imgX = x
+            if imgY < 0 or imgY >= w-1:
+              imgY = y
+            mean += vals[xVals][yVals] * image[imgX][imgY]
+      wResult[x][y] = mean
+  return wResult
+
+
+image = cv2.imread("Original.png")
+
+# Punto 2 mapeo directo con los valores
+# a = 2.1+2.1j; b = 0; c = 0.003; d = 1+1j
+imagen2 = mapeo_bilineal(image, 2.1+2.1j, 0, 0.003, 1+1j)
 cv2.imwrite("imagen2.png", imagen2)
+
+# Punto 3 mapeo inverso con los valores
+# a = 2.1+2.1j; b = 0; c = 0.003; d = 1+1j
+imagen3 = mapeo_inverso(image, 2.1+2.1j, 0, 0.003, 1+1j)
+cv2.imwrite("imagen3.png", imagen3)
+
+# Punto 4 mapeo inverso e interpolación en
+# colindancia N=4
+imagen4 = interpolacion(imagen3, 4)
+cv2.imwrite("imagen4.png", imagen4)
+
+# Punto 5 mapeo inverso e interpolación en
+# colindancia N=8
+imagen5 = interpolacion(imagen3, 8)
+cv2.imwrite("imagen5.png", imagen5)
+
+# Punto 6 imagen2 con filtro gausseano
+# máscara 5x5
+imagen6 = cv2.GaussianBlur(imagen2,(5,5),0)
+cv2.imwrite("imagen6.png", imagen6)
+
+# Punto 7 imagen4 con filtro gausseano
+# máscara 5x5
+imagen7 = cv2.GaussianBlur(imagen4,(5,5),0)
+cv2.imwrite("imagen7.png", imagen7)
+
+# Punto 8 imagen5 con filtro gausseano
+# máscara 5x5
+imagen8 = cv2.GaussianBlur(imagen5,(5,5),0)
+cv2.imwrite("imagen8.png", imagen8)
